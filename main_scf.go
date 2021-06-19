da7b7000e27b0035f6e59f73b6d5930f7e59743b
blue459 <blueinblockchain@163.com>
2021-06-19 11:29:06 +0800

    add scf Cors

--- main_scf.go
+++ main_scf.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"context"
+	"github.com/gin-contrib/cors"
 	"log"
 	"satotx/controller"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	ginadapter "github.com/linthan/scf-go-api-proxy/gin"
@@ -16,7 +18,17 @@ var ginLambda *ginadapter.GinLambda
 func init() {
 	log.Println("Gin cold start")
 	router := gin.Default()
-
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "OPTIONS"},
+		AllowHeaders:     []string{"Content-Type,Origin,Content-Encoding"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return true
+		},
+		MaxAge: 12 * time.Hour,
+	}))
 	router.GET("/", controller.Satotx)
 	router.POST("/utxo/:txid/:index", controller.SignUtxo)
 	router.POST("/utxo-spend-by/:txid/:index/:byTxid", controller.SignUtxoSpendBy)
